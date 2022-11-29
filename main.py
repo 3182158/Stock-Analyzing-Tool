9054ab1a081102c0ff028e36fef73313522c5525
3182158 <amritpal.singh01@nagarro.com>
2022-11-29 07:50:27 +0530

    Project init

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def run():
+    #game = SnakesAndLadder()
+    #game.view()
+    print('hello')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    run()
